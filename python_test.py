48325b1cf97895920ed714b034b0d689589090d5
cw-suzuki <suzuki.wataru@yes.nttcom.ne.jp>
2014-11-14 14:36:54 +0900

    シンタックスエラーソース追加

--- python_test.py
+++ python_test.py
@@ -1,24 +1,17 @@
 # -*- coding: utf-8 -*-
 
-def test1(arg):
+import sys, os
 
-	pirnt '------------------------------------'
-	pirnt '------------------------------------'
-	pirnt '------------------------------------'
-	pirnt '------------------------------------'
+def list_files(path):
+    for root, dirs, files in os.walk(path):
+        for filename in files:
+            fullname = os.path.join(root, filename)
+            print( fullname )
 
+def main(args):
+    path = args[1]
+    list_files(path)
 
-def test2(arg):
-
-	pirnt '------------------------------------'
-	pirnt '------------------------------------'
-	pirnt '------------------------------------'
-	pirnt '------------------------------------'
-
-def test3(arg):
-
-	pirnt '------------------------------------'
-	pirnt 'a'
-	pirnt '------------------------------------'
-	pirnt '------------------------------------'
-
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv)
